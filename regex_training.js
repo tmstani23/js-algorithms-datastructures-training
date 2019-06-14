d45b11c3b0ba7c0d2d6634353cefeb741ad66aab
Tim Work Laptop <tmstani23@gmail.com>
2019-06-14 15:38:25 -0500

    Update 1
    Added two regex examples.  One is for searching a single word and another for searching using or for multiple words in a string.

--- regex_training.js
+++ regex_training.js
@@ -1,1 +1,12 @@
-console.log("regex!")
+//Simple example using regex test method to test if the string contains the pattern: Hello
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+const result = myRegex.test(myString); 
+console.log(result, " search for single words");
+
+//Using | symbol to search string for any of the words
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/;
+let multipleResults = petRegex.test(petString);
+
+console.log(multipleResults, "searching for many words using or operator");
