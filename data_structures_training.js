f0eaabc70bf1e5f85782a3f7ffb37d7a54650b7a
Tim Work Laptop <tmstani23@gmail.com>
2019-07-16 15:36:11 -0500

    Update 3
    Added functions demonstrating spread operator and loops, indexOf and began work on demonstrating nested arrays.

--- data_structures_training.js
+++ data_structures_training.js
@@ -51,3 +51,63 @@ function forecast(arr) {
   return newArr;
 }
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+//Using spread operator to copy all elements of an array into a new array
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    // All elements of the input array are copied with spread operator and pushed into newArr
+    newArr.push([...arr]);
+    num--;
+  }
+  return newArr;
+}
+console.log("Using spread operator to copy all elements of an array into a new array", JSON.stringify(copyMachine([true, false, true], 2)));
+
+//Combining arrays with spread operator
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  //Here the fragment array is inserted into the sentence array
+  let sentence = ['learning', ...fragment, 'is', 'fun']; // change this line
+  return sentence;
+}
+console.log('combining arrays with spread operator', spreadOut());
+
+//Checking if an element exists within an array using indexOf
+function quickCheck(arr, elem) {
+  // Check if 'mushrooms' exists in the array and return true if it does else return false
+  return arr.indexOf(elem) != -1 ? true : false
+}
+console.log('checking if an elem exists in an array', quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+//Using loops to iterate through an array
+function filteredArray(arr, elem) {
+  let newArr = [];
+  // Loop through the array and check if current iterator contains input elem
+  for (let i = 0; i < arr.length; i++) {
+    //Push nested array to new array if elem is not found
+    arr[i].indexOf(elem) == -1 ? newArr.push(arr[i]) : null
+  }
+  return newArr;
+}
+
+// change code here to test different cases:
+//console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+console.log("Using loops to iterate through nested arrays", JSON.stringify(filteredArray([ ["trumpets", 2], ["flutes", 4], ["saxophones", 2] ], 2)))
+
+let nestedArray = [
+  // change code below this line
+  [
+    ['deep'],
+    [
+      ['deeper'],
+      [
+        [
+          'deepest'
+        ]
+      ]
+    ]
+  ]
+  // change code above this line
+];
+console.log(JSON.stringify(nestedArray[0]))
